95f24b6fc662b42488a9ab7a4757135e24ea8ccd
Martin Gonzalez <marttingonzalez@gmail.com>
2021-08-09 15:32:20 -0300

    Maquetacion Payment y Success

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../styles/components/Payment.css';
 
 const Payment = () => {
     return (
-       <h1>Payment</h1>
+       <div className="Payment">
+           <div className="Payment-content">
+               <h2>Resumen del pedido:</h2>
+               <div className="Payment-button">
+                   Boton de pago con Paypal
+               </div>
+           </div>
+       </div>
     );
 }
 
